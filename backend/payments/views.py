f4b6cabdd715a801e934fc41be4756b7e2f1b6bd
[sundeo] <[sundaydeogratias8@gmail.com]>
2022-01-07 21:18:52 +0300

    Address functionality added in the backend to link to braintree and database. Product details added to home UI

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Customer
+from .models import Customer, Address
 import braintree
 
 gateway = braintree.BraintreeGateway(
@@ -97,8 +97,96 @@ class ProcessPaymentView(APIView):
             {'error':'Failed to create customer'},
             status = status.HTTP_400_BAD_REQUEST
           )
+
+      if Address.objects.filter(
+        customer = customer,
+        street_address = street_address,
+        city = city,
+        country = country_name,
+        state_province = state_province,
+        postal_zip_code = postal_zip_code).exists():
+          address = Address.objects.get(
+            customer = customer,
+            street_address = street_address,
+            city = city,
+            country = country_name,
+            state_province = state_province,
+            postal_zip_code = postal_zip_code
+          )
+
+          adress_id = address.address_id
+          
+          try:
+            result = gateway.address.find(customer_id, adress_id)
+          except:
+            result= gateway.address.create({
+              'customer_id':customer_id,
+              'first_name':first_name,
+              'street_address':street_address,
+              'locality':city,
+              'region': state_province,
+              'postal_code': postal_zip_code,
+              'country_name': country_name,
+              'country_code_alpha2': country_code
+            })
+
+            if result.is_success:
+              address_id = str(result.address.id)
+
+              Address.objects.filter(
+                customer = customer,
+                street_address = street_address,
+                city = city,
+                country = country_name,
+                state_province = state_province,
+                postal_zip_code = postal_zip_code
+              ).update(address_id=address_id)
+            else:
+              return Response(
+                {'error':'Address invalid'},
+                status = status.HTTP_400_BAD_REQUEST
+              )
+      else:
+        result = gateway.address.create({
+          'customer_id':customer_id,
+          'first_name':first_name,
+          'street_address':street_address,
+          'locality':city,
+          'region': state_province,
+          'postal_code': postal_zip_code,
+          'country_name': country_name,
+          'country_code_alpha2': country_code
+        })
+
+        if result.is_success:
+          address_id = str(result.address.id)
+
+          Address.objects.create(
+            customer = customer,
+            address_id = address_id,
+            street_address = street_address,
+            city = city,
+            country = country_name,
+            state_province = state_province,
+            postal_zip_code = postal_zip_code
+          )
+
+          address = Address.objects.get(
+            customer = customer,
+            address_id = address_id,
+            street_address = street_address,
+            city = city,
+            country = country_name,
+            state_province = state_province,
+            postal_zip_code = postal_zip_code
+          )
+        else:
+          return Response(
+            {'error':'Failed to create address'},
+            status = status.HTTP_400_BAD_REQUEST
+          )
       return Response(
-        {'success': 'Created customer successfully'},
+        {'success': 'Created customer and address successfully'},
         status=status.HTTP_201_CREATED
       )
     except:
--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,10 +1,37 @@
 import React from "react";
 import Layout from "../hocs/Layout";
+import { Link } from "react-router-dom";
+
+import Groceries from "../assets/images/groceries.jpg";
 
 const Home = () => {
   return (
     <Layout title="Home" content="">
-      <h1>Home</h1>
+      <h1 className="display-4 mt-5">Shop</h1>
+      <h2 className="fs-4 fw-light fst-italic mt-3">
+        Where you can buy the worlds greatest groceries...
+      </h2>
+      <div className="mt-5 bg-light p-5 rounded shadow">
+        <h3 className="display-6">Check out Our Unreal Groceries box</h3>
+        <p className="lead mt-3 mb-4">A box containing fresh items</p>
+        <Link to="/checkout" className="btn btn-info shadow-sm">
+          I WANT THESE GROCERIES
+        </Link>
+      </div>
+      <div className="row mt-5 mb-5">
+        <div className="col-3">
+          <img className="img-fluid" src={Groceries} alt="Shop Groceries" />
+        </div>
+        <div className="offset-2 col-7">
+          <p className="lead mt-3 mb-4">A box contains: </p>
+          <ul className="list-group list-group-flush mt-5">
+            <li className="list-group-item fs-4 lead">Matooke</li>
+            <li className="list-group-item fs-4 lead">Sugar</li>
+            <li className="list-group-item fs-4 lead">Irish</li>
+            <li className="list-group-item fs-4 lead">Vegetables</li>
+          </ul>
+        </div>
+      </div>
     </Layout>
   );
 };
