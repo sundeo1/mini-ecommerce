8d867e5c311f3e0090d8620e9e1e2e7f9a1c7f05
[sundeo] <[sundaydeogratias8@gmail.com]>
2022-02-15 13:50:25 +0300

    Folder change

---
+++ payments/views.py
@@ -0,0 +1,248 @@
+from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Customer, Address, PaymentMethod, Order
+import braintree
+
+gateway = braintree.BraintreeGateway(
+  braintree.Configuration(
+    environment=braintree.Environment.Sandbox,
+    merchant_id= settings.BT_MERCHANT_ID,
+    public_key= settings.BT_PUBLIC_KEY,
+    private_key= settings.BT_PRIVATE_KEY
+  )
+)
+
+class GenerateTokenView(APIView):
+  def get(self, request, format=None):
+    try:
+      client_token = gateway.client_token.generate()
+
+      return Response(
+        {'token':client_token},
+        status=status.HTTP_200_OK
+      )
+    except Exception as e:
+      print(e)
+      return Response(
+        {'error':'Something went wrong when retrieving braintree token'},
+        status=status.HTTP_500_INTERNAL_SERVER_ERROR
+      )
+
+class ProcessPaymentView(APIView):
+  def post(self, request):
+    try:
+      data = request.data
+
+      first_name = data['first_name']
+      email = str(data['email'])
+      email = email.lower()
+
+      street_address = data['street_address']
+      city = data['city']
+      country = data['country']
+      state_province = data['state_province']
+      postal_zip_code = data['postal_zip_code']
+      nonce = data['nonce']
+
+      if country == 'uganda':
+        country_name = 'Uganda'
+        country_code = 'UG'
+      else:
+        country_name = 'Canada'
+        country_code = 'CA'
+
+      total_amount = '47.99'
+
+      if Customer.objects.filter(email=email).exists(): #Check if customer exists in database
+        customer = Customer.objects.get(email=email)
+        customer_id = str(customer.customer_id)
+
+        try:
+          gateway.customer.find(customer_id) #if works, customer exists in braintree
+        except:
+          result = gateway.customer.create({ #Otherwise, add customer to braintree
+            'first_name': first_name,
+            'email': email
+          })
+
+          if result.is_success:
+            customer_id = str(result.customer.id)
+
+            Customer.objects.filter(email=email).update(customer_id=customer_id)
+            customer = Customer.objects.get(email=email)
+          else:
+            return Response(
+              {'error':'Customer information invalid'},
+              status=status.HTTP_400_BAD_REQUEST
+            )
+
+      else:
+        result = gateway.customer.create({
+          'first_name': first_name,
+          'email': email
+        })
+
+        if result.is_success:
+          customer_id = str(result.customer.id)
+
+          customer= Customer.objects.create(
+            first_name = first_name,
+            email = email,
+            customer_id = customer_id
+          )
+        else:
+          return Response(
+            {'error':'Failed to create customer'},
+            status = status.HTTP_400_BAD_REQUEST
+          )
+
+      if Address.objects.filter(
+        customer = customer,
+        street_address = street_address,
+        city = city,
+        country = country_name,
+        state_province = state_province,
+        postal_zip_code = postal_zip_code).exists():
+          address = Address.objects.get(
+            customer = customer,
+            street_address = street_address,
+            city = city,
+            country = country_name,
+            state_province = state_province,
+            postal_zip_code = postal_zip_code
+          )
+
+          adress_id = address.address_id
+          
+          try:
+            result = gateway.address.find(customer_id, adress_id)
+          except:
+            result= gateway.address.create({
+              'customer_id':customer_id,
+              'first_name':first_name,
+              'street_address':street_address,
+              'locality':city,
+              'region': state_province,
+              'postal_code': postal_zip_code,
+              'country_name': country_name,
+              'country_code_alpha2': country_code
+            })
+
+            if result.is_success:
+              address_id = str(result.address.id)
+
+              Address.objects.filter(
+                customer = customer,
+                street_address = street_address,
+                city = city,
+                country = country_name,
+                state_province = state_province,
+                postal_zip_code = postal_zip_code
+              ).update(address_id=address_id)
+            else:
+              return Response(
+                {'error':'Address invalid'},
+                status = status.HTTP_400_BAD_REQUEST
+              )
+      else:
+        result = gateway.address.create({
+          'customer_id':customer_id,
+          'first_name':first_name,
+          'street_address':street_address,
+          'locality':city,
+          'region': state_province,
+          'postal_code': postal_zip_code,
+          'country_name': country_name,
+          'country_code_alpha2': country_code
+        })
+
+        if result.is_success:
+          address_id = str(result.address.id)
+
+          Address.objects.create(
+            customer = customer,
+            address_id = address_id,
+            street_address = street_address,
+            city = city,
+            country = country_name,
+            state_province = state_province,
+            postal_zip_code = postal_zip_code
+          )
+
+          address = Address.objects.get(
+            customer = customer,
+            address_id = address_id,
+            street_address = street_address,
+            city = city,
+            country = country_name,
+            state_province = state_province,
+            postal_zip_code = postal_zip_code
+          )
+        else:
+          return Response(
+            {'error':'Failed to create address'},
+            status = status.HTTP_400_BAD_REQUEST
+          )
+      
+      result = gateway.payment_method.create({ #Create Braintree payment method
+        'customer_id':str(customer_id),
+        'billing_address_id': address_id,
+        'payment_method_nonce': nonce
+      })
+
+      if result.is_success:
+        token = str(result.payment_method.token)
+
+        PaymentMethod.objects.create(
+          customer = customer,
+          billing_address = address,
+          token = token
+        )
+
+        payment_method = PaymentMethod.objects.get(
+          customer = customer,
+          billing_address = address,
+          token = token
+        )
+      else:
+        return Response(
+          {'error','Failed to pay create payment method' },
+          status = status.HTTP_400_BAD_REQUEST
+        )
+
+      result = gateway.transaction.sale({#Create Braintree transaction
+        'customer_id': str(customer_id),
+        'amount': total_amount,
+        'payment_method_token': token,
+        'billing_address_id': address_id,
+        'shipping_address_id': address_id,
+        'options': {
+          'submit_for_settlement': True
+        }
+      })
+
+      if result.is_success:
+        transaction_id = str(result.transaction.id)
+
+        Order.objects.create(
+          transaction_id = transaction_id,
+          customer = customer,
+          address = address,
+          payment_method = payment_method
+        )
+        return Response(
+        {'success': 'Transaction succesful'},
+        status=status.HTTP_201_CREATED
+      )
+      else:
+        return Response(
+          {'error':'Failed to process transactions' },
+          status=status.HTTP_400_BAD_REQUEST
+        )   
+    except:
+      return Response(
+        {'error':'Something went wrong when processing payment'},
+        status=status.HTTP_500_INTERNAL_SERVER_ERROR
+      )
---
+++ shop/settings.py
@@ -0,0 +1,138 @@
+
+import os
+import django_heroku
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'payments',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'shop.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR), 'build'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'shop.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'postgres',
+        'PASSWORD': 'sundeo',
+        'HOST': 'localhost'
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+# TIME_ZONE = 'UTC'
+TIME_ZONE='Africa/Kampala'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+BT_MERCHANT_ID = 'qrr7nkdb92j6tn6k'
+BT_PUBLIC_KEY = 'c4b4v64cmf3975mg'
+BT_PRIVATE_KEY = '5c6c31db6f2721f7b6f0a917df7f9d16'
+
+#CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
